83dd56e9efec9d5d08ee7cfd02c14328988a1a92
Steven Penny <svnpenn@gmail.com>
2016-12-12 21:41:08 -0600

    sage list | grep -Fvxf "$j" | xargs sage remove

--- sage-prune.sh
+++ sage-prune.sh
@@ -12,5 +12,5 @@ xc() {
 }
 j=$(mktemp)
 sage category Base | xargs sage depends | awk '$0=$NF' | sort -u > "$j"
-sage list | grep -vxf "$j" | xargs sage remove
+sage list | grep -Fvxf "$j" | xargs sage remove
 xc rm -rf /usr/x86_64-w64-mingw32
