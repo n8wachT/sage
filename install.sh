619d5799896045a4cb086f158122f2508d10bdc7
Steven Penny <svnpenn@gmail.com>
2017-01-21 20:49:48 -0600

    "$PWD"/sage-prune.sh "$PWD"/sage-spy.awk /usr/local/bin

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/dash -e
 ln -sfv "$PWD"/sage "$PWD"/sage-cost.sh "$PWD"/sage-date.awk \
-  "$PWD"/sage-prune.sh "$PWD"/sage-spy.sh /usr/local/bin
+  "$PWD"/sage-prune.sh "$PWD"/sage-spy.awk /usr/local/bin
 mkdir -pv /usr/share/doc/sage
 ln -sfv "$PWD"/sage.txt /usr/share/doc/sage
