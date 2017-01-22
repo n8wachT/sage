a07ab29bdd994a0b67478b424efc7650992d885c
Steven Penny <svnpenn@gmail.com>
2017-01-22 14:53:40 -0600

    t/searchall: stdlib

--- t/searchall.sh
+++ t/searchall.sh
@@ -1,12 +1,6 @@
-#!/bin/dash
-pause() {
-  echo 'Press any key to continue . . .'
-  read _
-  printf '\33c'
-}
+#!/bin/dash -e
+. ./stdlib.sh
 
-sage searchall
-pause
-sage searchall usr/bin/awj
-pause
-sage searchall usr/bin/awk
+xtrace sage searchall
+xtrace sage searchall usr/bin/awj
+xtrace sage searchall usr/bin/awk
