9fa1450c258695ecd95cf1078acebc7b63a6de95
Steven Penny <svnpenn@gmail.com>
2017-01-22 14:44:59 -0600

    t/download: stdlib

--- t/download.sh
+++ t/download.sh
@@ -1,27 +1,20 @@
 #!/bin/dash -e
-
-xc() {
-  awk 'BEGIN {d = "\47"; printf "\33[36m"; while (++j < ARGC) {
-  k = split(ARGV[j], q, d); q[1]; for (x in q) printf "%s%s",
-  q[x] ~ /^[[:alnum:]%+,./:=@_-]+$/ ? q[x] : d q[x] d, x < k ? "\\" d : ""
-  printf j == ARGC - 1 ? "\33[m\n" : FS}}' "$@"
-  "$@"
-}
+. ./stdlib.sh
 
 for q in /usr/local/http*/x86_64/release/make/make-*.tar.xz
 do :
 done
-xc sage download
+xtrace sage download
 
-xc sage download mak
+xtrace sage download mak
 
 # exist=0
-xc rm -fv "$q"
-xc sage download make
+xtrace rm -fv "$q"
+xtrace sage download make
 
 # exist=1 sha=0
-xc truncate -s0 "$q"
-xc sage download make
+xtrace truncate -s0 "$q"
+xtrace sage download make
 
 # exist=1 sha=1
-xc sage download make
+xtrace sage download make
