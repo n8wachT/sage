0b2bb9d55f033ae811ab75001453ad2359b54513
Steven Penny <svnpenn@gmail.com>
2018-01-13 11:40:33 -0600

    sh_trace rm -frv /usr/local/lib /usr/local/share/man /usr/x86_

--- sage-prune.sh
+++ sage-prune.sh
@@ -2,5 +2,5 @@
 z=$(mktemp)
 sage category Base | xargs sage depends | awk '$0=$NF' | sort -u > "$z"
 sage list | awk '$0=$1' FS='-[[:digit:]]' | grep -Fvxf "$z" | xargs sage remove
-sh_trace rm -frv /usr/local/lib /usr/x86_64-w64-mingw32
+sh_trace rm -frv /usr/local/lib /usr/local/share/man /usr/x86_64-w64-mingw32
 sh_trace find ~ -mindepth 1 -maxdepth 1 -type d -exec rm -frv {} +
