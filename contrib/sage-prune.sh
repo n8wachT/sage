2b58a238cec7a1c3b94a4eac158d2f824d0e8d28
Steven Penny <svnpenn@gmail.com>
2018-04-28 16:38:06 -0500

    k-trace rm -frv /usr/local/include /usr/local/lib /usr/local/

--- contrib/sage-prune.sh
+++ contrib/sage-prune.sh
@@ -2,6 +2,6 @@
 z=$(mktemp)
 sage category Base | xargs sage depends | awk '$0=$NF' | sort -u > "$z"
 sage list | awk '$0=$1' FS='-[[:digit:]]' | grep -Fvxf "$z" | xargs sage remove
-k-trace rm -frv /usr/local/include /usr/local/lib /usr/local/share/man \
-  /usr/x86_64-w64-mingw32
+k-trace rm -frv /usr/local/include /usr/local/lib /usr/local/share/doc \
+  /usr/local/share/man /usr/x86_64-w64-mingw32
 k-trace find ~ -mindepth 1 -maxdepth 1 -type d -exec rm -frv {} +
